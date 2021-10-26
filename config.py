82e12c178eeaaf991f05e5219b9186723e9c73cd
NazihZaz <89110487+NazihZaz@users.noreply.github.com>
2021-10-26 19:44:06 -0400

    Updated config.py
    Updated config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Google API Key
-g_key = "AIzaSyDgVFL9ct5ShdlTATaSDcjo15TzvbJlRwo"
+g_key = "ENTER YOUR KEY HERE!"
 
 # COVID API Key
-api_key="68660b75f13344c3a7b6f6498081c1b7"
+api_key="ENTER YOUR KEY HERE!"
